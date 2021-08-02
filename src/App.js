b12ea72376ea428dd7063575ceab25af87f1d409
Your Name <shachinikarunarathne2016@gmail.com>
2021-08-03 01:31:06 +0530

    pracise project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import Navbar from './components/navbar';
+import EditExercise from './components/edit-exercise';
+import ExercisesList from './components/exercises-list';
+import CreateExercise from './components/create-exercise';
+import CreateUser from './components/create-user';
+
+function App() {
+
+  return (
+    <Router>
+     <div className="container">
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
+      
+    </div>
+    </Router>
+    
+  );
+}
+
+export default App;
